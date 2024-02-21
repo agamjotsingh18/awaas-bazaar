d0405cc186d8337d2002f5d24f29c2d80097cc3c
agamjotsingh18 <70067726+agamjotsingh18@users.noreply.github.com>
2024-02-21 21:44:00 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import NavBar from './NavBar';
 import SearchHeader from './SearchHeader';
 import CardsHeader from './CardsHeader';
+import { Box } from '@chakra-ui/react';
 
 const Home = () => {
   const [selectedPropertyType, setSelectedPropertyType] = useState('');
@@ -12,9 +13,17 @@ const Home = () => {
         <NavBar setSelectedPropertyType={setSelectedPropertyType} />
         <SearchHeader />
         <CardsHeader selectedPropertyType={selectedPropertyType} />
-        <div style={{ marginTop: 'auto', textAlign: 'center', padding: '1rem' }}>
+     
+        <Box
+          mt="auto"
+          textAlign="center"
+          padding="1rem"
+          borderTop="1px"
+          borderColor="gray.200"
+          bg="gray.50"
+        >
           &copy; 2024 Awaas Bazaar. All rights reserved.
-        </div>
+        </Box>
       </div>
     </>
   );
